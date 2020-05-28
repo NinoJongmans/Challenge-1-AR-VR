f5cf5483152d10ca9d4c27c2bcb4ef7eff966b62
Nino Jongmans <ninojongmans@gmail.com>
2020-05-28 13:34:40 +0200

    4

--- js/main.js
+++ js/main.js
@@ -1,17 +1,27 @@
 var extraction = document.getElementById("extraction"); 
 var playbtn = document.getElementById("bordbegin");
 var stopbtn = document.getElementById("bordstop");
-
-
+var plafondlamp = document.getElementById('plafondlamp');
+var plafondlamp1 = document.getElementById('plafondlamp1');
+var plafondlamp2 = document.getElementById('plafondlamp2');
+var plafondlamp3 = document.getElementById('plafondlamp3');
 
 
 document.getElementById('bordbegin').addEventListener('click', function() {
     console.log('Film gestart');
     document.getElementById('extraction').play();
+    plafondlamp.components.material.material.opacity = 1;
+    plafondlamp1.components.material.material.opacity = 1;
+    plafondlamp2.components.material.material.opacity = 1;
+    plafondlamp3.components.material.material.opacity = 1;
 });
 
 
 document.getElementById('bordstop').addEventListener('click', function() {
     console.log('Film gepauzeerd');
     document.getElementById('extraction').pause();
+    plafondlamp.components.material.material.opacity = 0.1;
+    plafondlamp1.components.material.material.opacity = 0.1;
+    plafondlamp2.components.material.material.opacity = 0.1;
+    plafondlamp3.components.material.material.opacity = 0.1;
 })
